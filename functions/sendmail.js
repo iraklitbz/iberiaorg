64781480ce630e47379613083c7a443257a7c842
irakli.tavberidze <irakli.tavberidze@webfg.com>
2021-09-30 17:08:18 +0200

    otro intentio

--- functions/sendmail.js
+++ functions/sendmail.js
@@ -17,33 +17,33 @@ exports.handler = function(event, context, callback) {
         from: data.email,
         to: process.env.email,
         subject: `Email recibido de ${data.email} desde el formulario de la web`,
-    html: `
-            <table style="min-width:348px" width="100%;" height="100%" cellspacing="0" cellpadding="0" border="0">
-            <tbody style="background-color: #f6f6f6;">
-              <tr style="height:32px" height="32"><td></td></tr> 
-              <tr align="center">
-                  <td>
-                    <table style="padding-bottom: 20px; width: 516px;min-width: 220px;" cellspacing="0" cellpadding="0" border="0"> 
-                      <tr>
-                        <td style="width:8px" width="8"></td>
-                          <td>
-                              <div style="background-color: #fff; border-style: solid;border-width: thin;border-color: #f3f3f3;border-radius: 8px;padding: 40px 20px;">
-                              <div style="font-size:20px; padding-bottom: 20px; text-align: center; border-bottom: thin solid #f3f3f3;"> <b> ${data.email} </b> pregunta lo siguiente: <br/> </div>
-                              <div style="font-family:Roboto-Regular,Helvetica,Arial,sans-serif;font-size:16px;color:rgba(0,0,0,0.87);line-height:20px;padding-top:20px;text-align:left">
-                                ${data.texto}
-                              </div>
-                            </div>
-                          
-                          </td>
-                        <td style="width:8px" width="8"></td>
-                      </tr>
-                    </table>
-                  </td>
-                </tr>
-                <tr style="height:32px" height="32"><td></td></tr>
-              </tbody>
-            </table>  
-          `
+        html: `
+                <table style="min-width:348px" width="100%;" height="100%" cellspacing="0" cellpadding="0" border="0">
+                <tbody style="background-color: #f6f6f6;">
+                  <tr style="height:32px" height="32"><td></td></tr> 
+                  <tr align="center">
+                      <td>
+                        <table style="padding-bottom: 20px; width: 516px;min-width: 220px;" cellspacing="0" cellpadding="0" border="0"> 
+                          <tr>
+                            <td style="width:8px" width="8"></td>
+                              <td>
+                                  <div style="background-color: #fff; border-style: solid;border-width: thin;border-color: #f3f3f3;border-radius: 8px;padding: 40px 20px;">
+                                  <div style="font-size:20px; padding-bottom: 20px; text-align: center; border-bottom: thin solid #f3f3f3;"> <b> ${data.email} </b> pregunta lo siguiente: <br/> </div>
+                                  <div style="font-family:Roboto-Regular,Helvetica,Arial,sans-serif;font-size:16px;color:rgba(0,0,0,0.87);line-height:20px;padding-top:20px;text-align:left">
+                                    ${data.texto}
+                                  </div>
+                                </div>
+                              
+                              </td>
+                            <td style="width:8px" width="8"></td>
+                          </tr>
+                        </table>
+                      </td>
+                    </tr>
+                    <tr style="height:32px" height="32"><td></td></tr>
+                  </tbody>
+                </table>  
+              `
     }, function(error, info) {
         if (error) {
             callback(error);
---
+++ pages/api/contact.js
@@ -0,0 +1,61 @@
+export default function (req, res) {
+  require('dotenv').config()
+
+  let nodemailer = require('nodemailer')
+  const transporter = nodemailer.createTransport({
+    port: 465,     
+    host: "smtp.gmail.com",
+       auth: {
+            user: process.env.email,
+            clientId: '000000000000-xxx0.apps.googleusercontent.com',
+            clientSecret: 'XxxxxXXxX0xxxxxxxx0XXxX0',
+            refreshToken: '1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx',
+            accessToken: 'ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x',
+            expires: 1484314697598
+         },
+    secure: true,
+  });
+  
+  const mailData = {
+    from: req.body.email, // TODO: email sender
+    to: process.env.email, // TODO: email receiver
+    subject: `Email recibido de ${req.body.email} desde el formulario de la web`,
+    html: `
+            <table style="min-width:348px" width="100%;" height="100%" cellspacing="0" cellpadding="0" border="0">
+            <tbody style="background-color: #f6f6f6;">
+              <tr style="height:32px" height="32"><td></td></tr> 
+              <tr align="center">
+                  <td>
+                    <table style="padding-bottom: 20px; width: 516px;min-width: 220px;" cellspacing="0" cellpadding="0" border="0"> 
+                      <tr>
+                        <td style="width:8px" width="8"></td>
+                          <td>
+                              <div style="background-color: #fff; border-style: solid;border-width: thin;border-color: #f3f3f3;border-radius: 8px;padding: 40px 20px;">
+                              <div style="font-size:20px; padding-bottom: 20px; text-align: center; border-bottom: thin solid #f3f3f3;"> <b> ${req.body.email} </b> pregunta lo siguiente: <br/> </div>
+                              <div style="font-family:Roboto-Regular,Helvetica,Arial,sans-serif;font-size:16px;color:rgba(0,0,0,0.87);line-height:20px;padding-top:20px;text-align:left">
+                                ${req.body.texto}
+                              </div>
+                            </div>
+                          
+                          </td>
+                        <td style="width:8px" width="8"></td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+                <tr style="height:32px" height="32"><td></td></tr>
+              </tbody>
+            </table>  
+          `
+  }
+
+  transporter.sendMail(mailData, function (err, info) {
+      if(err)
+        console.log(err)
+      else
+        console.log(info);
+  })
+
+  console.log(req.body)
+  res.send('success')
+}
