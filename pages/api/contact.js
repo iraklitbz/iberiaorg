5bb74a8122e771e7f2c566eb25115046138f8401
irakli.tavberidze <irakli.tavberidze@webfg.com>
2021-09-30 17:11:43 +0200

    css

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -7,13 +7,9 @@ export default function (req, res) {
     host: "smtp.gmail.com",
        auth: {
             user: process.env.email,
-            clientId: '000000000000-xxx0.apps.googleusercontent.com',
-            clientSecret: 'XxxxxXXxX0xxxxxxxx0XXxX0',
-            refreshToken: '1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx',
-            accessToken: 'ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x',
-            expires: 1484314697598
+            pass: process.env.password,
          },
-    secure: true,
+    secure: false,
   });
   
   const mailData = {
