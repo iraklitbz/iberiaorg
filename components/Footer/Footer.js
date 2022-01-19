c21effabf8d1e289a2448c8cc61901731c5717a4
irakli.tavberidze <irakli.tavberidze@webfg.com>
2022-01-19 15:08:35 +0100

    footer changed

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -23,7 +23,7 @@ const Footer = () => {
            
 
             <div className="main-footer__colophon border-top padding-top-xs">
-              <div className="text-sm text-xs@md color-contrast-medium flex gap-xs">
+              <div className="text-sm text-xs@md color-contrast-medium flex-column flex-row@sm flex gap-xs">
                 <span>©<strong>VESTA</strong> ADMINISTRACIONES</span>
                 <Link href="/politica-de-privacidad" className="color-contrast-high">Política de privacidad</Link>
                 <Link href="/politica-cookies" className="color-contrast-high">Política cookies</Link>
