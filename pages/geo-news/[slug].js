4df9b046d00060876a6b2724a3cd3268e90fc05e
irakli.tavberidze <irakli.tavberidze@webfg.com>
2022-03-23 17:41:30 +0100

    added useeffect

--- pages/geo-news/[slug].js
+++ pages/geo-news/[slug].js
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React, { useEffect } from "react";
 import MainLayout from "../../layouts/MainLayout";
 import { client } from "../../lib/wordpress/client";
 import {
