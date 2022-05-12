ad1ba61e92d04e36bcf5e2f784eac33aa9a2082a
iraklitbz <iraklitbz@gmail.com>
2022-05-12 14:53:48 +0200

    added image for index by default

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ export default function Home({posts}) {
   return (
 
       <MainLayout
+        previewImage='https://iberiainfo.me/wp-content/uploads/2022/02/logo_iberia_icon.svg'
         pageTitle={intl.formatMessage({ id: "indexTitle" })}
         description={intl.formatMessage({ id: "indexDescription" })}
       > 
